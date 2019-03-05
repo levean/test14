cd51c1be01d484e2239cea81386a337e1937d644
xw <824526506@qq.com>
2019-03-05 17:28:55 +0800

    fix:完善启动类打包

--- src/main/java/com/zfkr/Test14Application.java
+++ src/main/java/com/zfkr/Test14Application.java
@@ -3,10 +3,11 @@ package com.zfkr;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 @MapperScan("com.zfkr.**.dao")
-public class Test14Application {
+public class Test14Application extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(Test14Application.class, args);
